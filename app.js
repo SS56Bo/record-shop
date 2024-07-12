42b8ae39b19a5ad8da23a9f1e55830a8be32fbb9
SS56Bo <sohamsarkar560@gmail.com>
2024-07-13 01:40:10 +0530

    Added extra few steps

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ const albumRouter = require('./routes/albumRoute');
 
 app.use(express.json());
 
-////// routes ////
-//app.get("/api/v1/albums", getAllAlbums);
-//app.post("/api/v1/albums", postNewAlbum);
-// app.get("/api/v1/albums/:id", getSingleAlbum);
-// app.patch("/api/v1/albums/:id", updateAlbum);
-// app.delete("/api/v1/albums/:id", deleteAlbum);
-
 app.use('/api/v1/albums', albumRouter);
 
 const port = 3000;
