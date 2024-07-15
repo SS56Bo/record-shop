ef9808b36feaa537aa51b93b8cde8813bda21ca9
SS56Bo <sohamsarkar560@gmail.com>
2024-07-15 16:45:06 +0530

    Marked for Rollback in case of MongoDB error

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const morgan = require('morgan');
 const path = require('path');
 const albumRouter = require('./routes/albumRoute');
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/v1/albums', albumRouter);
