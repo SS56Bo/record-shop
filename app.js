0d346bb832ffd33e14edf364213cb1eff330487d
SS56Bo <sohamsarkar560@gmail.com>
2024-07-29 02:11:06 +0530

    Added some error handlers. Need to update them later. Moving on to user handling

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.use(express.json());
 app.use('/api/v1/albums', albumRouter);
 
 app.all('*', (req, res, next) => {
-  const err = new Error(`Can't find ${req.originalUrl} form this server !`);
-  (err.status = 'Fail !'), (err.statusCode = 404);
-  next(err);
+  // const err = new Error(`Can't find ${req.originalUrl} form this server !`);
+  // err.status = 'Fail !';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} form this server !`));
 });
 
 app.use(globalErrorHandler);
