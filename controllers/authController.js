e9801c0316bf9008360ef91d357f85d924e201b2
SS56Bo <sohamsarkar560@gmail.com>
2024-09-25 18:50:48 +0530

    Added HTML files for Templating

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
 const catchAsyncError = require('./../utils/catchAsyncError');
 
 exports.signup = catchAsyncError(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
+  let token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,8 +27,18 @@ const userSchema = new mongoose.Schema({
       validator: function (el) {
         return el === this.password;
       },
+      message: 'Passwords do not match!',
     },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const UserInfo = mongoose.model('Users', userSchema);
